e6b60486bbe73babc99a00ae3bd1b2b470708db3
yuri <maizenadiscord@gmail.com>
2022-06-17 22:53:42 -0300

    é tanta coisa que eu não vou nem descrever

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 const { Client } = require('discord.js');
-const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const http = require("http");
+const express = require('express')
+var cors = require('cors');
 require('dotenv').config();
+
+const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const app = express()
 
 const token = process.env.BOT_TOKEN;
 const channelID = process.env.CHANNEL_ID;
+const frontEndURL = process.env.FRONTEND_URL;
+const port = process.env.PORT;
 
-const host = 'localhost';
-const port = 8000;
+app.use(cors({
+    origin: frontEndURL
+}));
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
+app.get('/changelog', (req, res) => {
+    res.contentType("application/json");
+    console.log("Request received.");
     const channel = client.channels.fetch(channelID).then(channel => {
         channel.messages.fetch({ limit: 100 }).then((messages) => {
             res.write("[");
             messages.forEach(message => {
-                res.write(JSON.stringify(message));
+                //res.write(JSON.stringify(message));
+                let timestamp = message.createdTimestamp;
+                let date = new Date(timestamp);
+                let day = date.getDate();
+                let month = date.getMonth() + 1;
+                let year = date.getFullYear();
+                let hours = date.getHours();
+                let minutes = date.getMinutes();
+                let seconds = date.getSeconds();
+
+                let formattedTime = `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`;
+                let author = message.author.username;
+                let authorAvatar = message.author.avatarURL() || "https://cdn.discordapp.com/embed/avatars/0.png";
+                let content = message.content
+                    .replace(/\n/g, "<br>")
+                    .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>")
+                    .replace(/<br>-<br>/g, "<br>")
+                    .replace(/@everyone/g, "<strong>@everyone</strong>")
+                    .replace(/@here/g, "<strong>@here</strong>")
+                    .replace(/<@\d+>/g, (mention) => {
+                        let id = mention.replace(/<@|>/g, "");
+                        let username = client.users.cache.get(id).username;
+                        return "<strong>@" + username + "</strong>";
+                    });
+
+                let attachments = message.attachments.map(attachment => {
+                    return attachment.url;
+                });
+
+                res.write(`{"date":"${formattedTime}","content":"${content}","attachments":${JSON.stringify(attachments)},"author":{"name":"${author}","avatar":"${authorAvatar}"}}`);
                 if (message.id != messages.last().id) {
                     res.write(",");
                 }
@@ -24,13 +59,11 @@ const requestListener = function (req, res) {
             res.end("]");
         });
     }).catch(console.error);
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
 });
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
